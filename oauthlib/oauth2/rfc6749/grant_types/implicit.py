ece4fac412afba80aa21e06bad646ebd7e355327
Ib Lundgren <ib.lundgren@gmail.com>
2014-07-06 18:20:36 +0100

    Move save token to grant type and allow token modification.
    request_validator.save_bearer_token was previously called from inside
    token_handler.create_token but is now called from the respective
    grant type after being created by token_handler.create_token.
    This makes it easier to allow extensions grants to modify the token
    via the newly introduced token modifier methods that will be invoked
    prior to the save.
    
    This is consistent with how auth code is created and saved plus it
    removes the hidden dual purpose of create_token.

--- oauthlib/oauth2/rfc6749/grant_types/implicit.py
+++ oauthlib/oauth2/rfc6749/grant_types/implicit.py
@@ -119,6 +119,14 @@ class ImplicitGrant(GrantTypeBase):
 
     def __init__(self, request_validator=None):
         self.request_validator = request_validator or RequestValidator()
+        self._token_modifiers = []
+        self.response_types = ['token']
+
+    def register_response_type(self, response_type):
+        self.response_types.append(response_type)
+
+    def register_token_modifier(self, modifier):
+        self._token_modifiers.append(modifier)
 
     def create_authorization_response(self, request, token_handler):
         """Create an authorization response.
@@ -229,6 +237,9 @@ class ImplicitGrant(GrantTypeBase):
                                                          fragment=True)}, None, 302
 
         token = token_handler.create_token(request, refresh_token=False)
+        for modifier in self._token_modifiers:
+            token = modifier(token)
+        self.request_validator.save_token(token, request)
         return {'Location': common.add_params_to_uri(request.redirect_uri, token.items(),
                                                      fragment=True)}, None, 302
 
@@ -318,9 +329,8 @@ class ImplicitGrant(GrantTypeBase):
         # REQUIRED.
         if request.response_type is None:
             raise errors.MissingResponseTypeError(request=request)
-        # Value MUST be set to "token".
-        elif request.response_type != 'token':
-            raise errors.UnsupportedResponseTypeError(request=request)
+        elif request.response_type not in self.response_types:
+            raise errors.UnsupportedResponseTypeError(state=request.state, request=request)
 
         log.debug('Validating use of response_type token for client %r (%r).',
                   request.client_id, request.client)
